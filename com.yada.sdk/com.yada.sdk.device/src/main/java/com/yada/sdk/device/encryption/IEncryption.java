dbb4fa048fb57feb5332d37b5765020e592f8d59
ZhangYaMin <ZhangYaMin@ZYM>
2014-07-16 17:41:05 +0800

    加密机核心SDK增加指令

--- com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/encryption/IEncryption.java
+++ com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/encryption/IEncryption.java
@@ -55,4 +55,71 @@ public interface IEncryption {
 	 * @return MAC
 	 */
 	public ByteBuffer getTakMac(ByteBuffer macData, String lmkTak);
+	
+	/***
+	 * 获取将ZPK保护的PIN密文转为受本地LMK密钥（LMK）保护的pin的密文
+	 * 
+	 * @param hsmZpkLmk
+	 *            受区域主密钥（LMK）保护的PIN主密钥（ZpkLmk）
+	 * @param classifiedPin
+	 * 				受本地PIN密钥（ZpkLmk）保护的pin的密文
+	 * @param cardNo
+	 * 				卡号/主账号
+	 * @return 受本地PIN密钥（ZpkLmk）保护的pin的明文
+	 * @throws Exception 
+	 */
+	public String getLmkPinFromZpkPin(String hsmZpkLmk, String classifiedPin, String cardNo);
+	
+	/***
+	 * 获取受主密钥（Lmk）保护的pin的明文
+	 * 
+	 * @param classifiedPin
+	 * 				主密钥（Lmk）保护的pin的密文
+	 * @param cardNo
+	 *            卡号/主账号
+	 * @return 受本地主密钥（Lmk）保护的pin的明文s
+	 * 
+	 * @author ZhangYaMin
+	 */
+	public String DecryptPin(String classifiedPin, String cardNo);
+	
+	/***
+	 * 获取受主密钥(LMK)保护的PIN密钥对
+	 * @param hsmKeyLmk
+	 * 				区域主密钥（LMK）
+	 * @return 返回PIN密钥对字符串
+	 * @author ZhangYaMin
+	 */
+	public String GetLmkZpk(String hsmKeyLmk);
+	
+	/***
+	 * 获取受主密钥(LMK)保护的MAC密钥对
+	 * @param hsmKeyLmk
+	 * 				区域主密钥（LMK）
+	 * @return 返回MAC密钥对字符串
+	 * @author ZhangYaMin
+	 */
+	public String GetLmkZak(String hsmKeyLmk);
+	
+	/***
+	 * 获取受主密钥(LMK)保护的ZEK密钥对
+	 * @param hsmKeyLmk
+	 * 				区域主密钥（LMK）
+	 * @return 返回ZEK密钥对字符串
+	 * @author ZhangYaMin
+	 */
+	public String GetLmkZek(String hsmKeyLmk);
+	
+	/***
+	 * 通过Zak获取MAC
+	 * 
+	 * @param lmkZak
+	 *            受本地主密钥（LMK）保护的密钥（ZAK）
+	 * @param macData
+	 *            mac验证的数据
+	 * @return MAC
+	 * @author ZhangYaMin
+	 */
+	public String getZakMac(String lmkZak, String macData);
+	
 }
--- com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/encryption/hsm/EncryptionMachine.java
+++ com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/encryption/hsm/EncryptionMachine.java
@@ -19,6 +19,12 @@ public class EncryptionMachine implements IEncryption {
 		this.lmkZmk = lmkZmk;
 		this.endPoint = new InetSocketAddress(serverIp, port);
 	}
+	
+	public EncryptionMachine(String serverIp, int port) {
+		this.endPoint = new InetSocketAddress(serverIp, port);
+	}
+
+
 
 	private String send(String reqMessage) {
 		ByteBuffer reqBuffer = ByteBuffer.wrap(reqMessage.getBytes());
@@ -198,5 +204,171 @@ public class EncryptionMachine implements IEncryption {
 
 		return ByteBuffer.wrap(macByte);
 	}
+
+	@Override
+	public String getLmkPinFromZpkPin(String hsmZpkLmk, String classifiedPin,
+			String cardNo)  {
+		StringBuilder sb = new StringBuilder();
+		// 1.消息头 2.命令代码
+		sb.append(messageHead).append("JE");
+		// 是否需要增加1A
+		if (hsmZpkLmk.length() == 16) {
+			sb.append(hsmZpkLmk);
+		}else if(hsmZpkLmk.length() == 32){
+			sb.append("X").append(hsmZpkLmk);
+		}else{
+			throw new RuntimeException("getLmkPinFromZpkPin()密钥长度错误...");
+		}
+		
+		
+		if(classifiedPin.length()==16 && cardNo.length() > 13){
+			sb.append(classifiedPin).append("01").append(cardNo.substring(cardNo.length()-13, cardNo.length()-1));
+		}else{
+			throw new RuntimeException("getLmkPinFromZpkPin()-pin密文/主账号长度错误...");
+		}
+		
+		String respMessage = send(sb.toString());
+		String respCode = respMessage.substring(messageHead.length()+2, messageHead.length()+2+2);
+		if(!respCode.equals("00")){
+			throw new RuntimeException("加密机返回失败！" + respCode);
+		}
+		String lmkPin = respMessage.substring(messageHead.length()+2+2);
+		
+		return lmkPin;
+	}
+	
+	@Override
+	public String DecryptPin(String classifiedPin, String cardNo) {
+		StringBuilder sb = new StringBuilder();
+		// 1.消息头 2.命令代码
+		sb.append(messageHead).append("NG");
+		
+		// 3.PAN 4.PIN密文
+		if(cardNo.length() > 13){
+			sb.append(cardNo.substring(cardNo.length()-13, cardNo.length()-1)).append(classifiedPin);
+		}else{
+			throw new RuntimeException("DecryptPin()-主账号长度错误...");
+		}
+		
+		String respMessage = send(sb.toString());
+		String respCode = respMessage.substring(messageHead.length()+2, messageHead.length()+2+2);
+		if(!respCode.equals("00")){
+			throw new RuntimeException("加密机返回失败！" + respCode);
+		}
+		//去F
+		String unClassifiedPin = respMessage.substring(messageHead.length()+2+2, messageHead.length()+2+2+6).replace("F", "");
+		
+		return unClassifiedPin;
+	}
+
+	@Override
+	public String GetLmkZpk(String hsmKeyLmk) {
+		StringBuilder sb = new StringBuilder();
+		// 1.消息头 2.命令代码
+		sb.append(messageHead).append("IA");
+		// 是否需要增加1A
+		if (hsmKeyLmk.length() == 16) {
+			sb.append(hsmKeyLmk);
+		}else if(hsmKeyLmk.length() == 32){
+			sb.append("X").append(hsmKeyLmk).append(";XX0");
+		}else{
+			throw new RuntimeException("GetLmkZpk()密钥长度错误...");
+		}
+		
+		String respMessage = send(sb.toString());
+		String respCode = respMessage.substring(messageHead.length()+2, messageHead.length()+2+2);
+		if(!respCode.equals("00")){
+			throw new RuntimeException("加密机返回失败！" + respCode);
+		}
+		String pinKeyInfo = respMessage.substring(messageHead.length()+2+2);
+		
+		return pinKeyInfo;
+	}
+
+	@Override
+	public String GetLmkZak(String hsmKeyLmk) {
+		StringBuilder sb = new StringBuilder();
+		// 1.消息头 2.命令代码 3.模式（1位）+key type(3位)
+		sb.append(messageHead).append("A0").append("1008");
+		// 4.Key scheme LMK 5.ZMK 6.Key scheme ZMK
+		if (hsmKeyLmk.length() == 16) {
+			sb.append("Z").append(hsmKeyLmk).append("Z");
+		}else if(hsmKeyLmk.length() == 32){
+			sb.append("X").append(hsmKeyLmk).append("X");
+		}else{
+			throw new RuntimeException("GetLmkZak()密钥长度错误...");
+		}
+		
+//		System.out.println(sb.toString());
+		
+		String respMessage = send(sb.toString());
+		
+//		System.out.println(respMessage);
+		
+		String respCode = respMessage.substring(messageHead.length()+2, messageHead.length()+2+2);
+		if(!respCode.equals("00")){
+			throw new RuntimeException("加密机返回失败！" + respCode);
+		}
+		String macKeyInfo = respMessage.substring(messageHead.length()+2+2);
+		
+		return macKeyInfo;
+	}
+
+	@Override
+	public String GetLmkZek(String hsmKeyLmk) {
+		StringBuilder sb = new StringBuilder();
+		// 1.消息头 2.命令代码 3.模式（1位）+key type(3位)
+		sb.append(messageHead).append("A0").append("100A");
+		// 4.Key scheme LMK 5.ZMK 6.Key scheme ZMK
+		if (hsmKeyLmk.length() == 16) {
+			sb.append("Z").append(hsmKeyLmk).append("Z");
+		}else if(hsmKeyLmk.length() == 32){
+			sb.append("X").append(hsmKeyLmk).append("X");
+		}else{
+			throw new RuntimeException("GetLmkZek()密钥长度错误...");
+		}
+		
+		String respMessage = send(sb.toString());
+		String respCode = respMessage.substring(messageHead.length()+2, messageHead.length()+2+2);
+		if(!respCode.equals("00")){
+			throw new RuntimeException("加密机返回失败！" + respCode);
+		}
+		String zekKeyInfo = respMessage.substring(messageHead.length()+2+2);
+		
+		return zekKeyInfo;
+	}
+
+	@Override
+	public String getZakMac(String lmkZak, String macData) {
+		StringBuilder sb = new StringBuilder();
+		// 1.消息头 2.命令代码 3.模式（1位）+key type(3位)
+		sb.append(messageHead).append("MS").append("01");
+		// 4.Key scheme LMK 5.ZMK 6.Key scheme ZMK
+		if (lmkZak.length() == 16) {
+			sb.append("00").append(lmkZak);
+		}else if(lmkZak.length() == 32){
+			sb.append("10X").append(lmkZak);
+		}else{
+			throw new RuntimeException("getZakMac()密钥长度错误...");
+		}
+		String len = String.format("%04X", macData.length());
+		sb.append(len).append(macData);
+		
+//		System.out.println(sb.toString());
+		
+		String respMessage = send(sb.toString());
+		String respCode = respMessage.substring(messageHead.length()+2, messageHead.length()+2+2);
+		
+//		System.out.println(respMessage);
+		
+		if(!respCode.equals("00")){
+			throw new RuntimeException("加密机返回失败！" + respCode);
+		}
+		
+		String macInfo = respMessage.substring(messageHead.length()+2+2);
+		return macInfo;
+	}
+
+	
 
 }
