ca3f06421afdfe44ae01913b50d05cd52608a930
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-05-24 14:55:03 +0800

    增加一个异常记录

--- com.yada.sdk/com.yada.sdk.net/src/main/java/com/yada/sdk/net/TcpService.java
+++ com.yada.sdk/com.yada.sdk.net/src/main/java/com/yada/sdk/net/TcpService.java
@@ -1,5 +1,8 @@
 package com.yada.sdk.net;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.AsynchronousChannelGroup;
@@ -11,121 +14,121 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 
  * @author tao
- * 
  */
 public final class TcpService {
+    private final static Logger logger = LoggerFactory
+            .getLogger(TcpService.class);
+    private AsynchronousServerSocketChannel server;
+    private String serviceName;
+    private AsynchronousChannelGroup channelGroup;
+    private ConcurrentLinkedQueue<DataTransceivers> dataTransceiversQueue;
+    private IPackageSplitterFactory splitterFactory;
+    private IPackageProcessorFactory processorFactory;
+    private long recvTimeout;
+    private volatile boolean isOpen = false;
 
-	private AsynchronousServerSocketChannel server;
-	private String serviceName;
-	private AsynchronousChannelGroup channelGroup;
-	private ConcurrentLinkedQueue<DataTransceivers> dataTransceiversQueue;
-	private IPackageSplitterFactory splitterFactory;
-	private IPackageProcessorFactory processorFactory;
-	private long recvTimeout;
-	private volatile boolean isOpen = false;
+    public TcpService(String serviceName,
+                      IPackageSplitterFactory splitterFactory,
+                      IPackageProcessorFactory processorFactory, long recvTimeout)
+            throws IOException {
+        this.serviceName = serviceName;
+        dataTransceiversQueue = new ConcurrentLinkedQueue<DataTransceivers>();
+        this.splitterFactory = splitterFactory;
+        this.processorFactory = processorFactory;
+        this.recvTimeout = recvTimeout;
+    }
 
-	public TcpService(String serviceName,
-			IPackageSplitterFactory splitterFactory,
-			IPackageProcessorFactory processorFactory, long recvTimeout)
-			throws IOException {
-		this.serviceName = serviceName;
-		dataTransceiversQueue = new ConcurrentLinkedQueue<DataTransceivers>();
-		this.splitterFactory = splitterFactory;
-		this.processorFactory = processorFactory;
-		this.recvTimeout = recvTimeout;
-	}
+    public synchronized void listen(InetSocketAddress endPoint)
+            throws IOException {
+        close();
+        channelGroup = AsynchronousChannelGroup.withFixedThreadPool(16,
+                Executors.defaultThreadFactory());
+        server = AsynchronousServerSocketChannel.open(channelGroup).bind(
+                endPoint);
 
-	public synchronized void listen(InetSocketAddress endPoint)
-			throws IOException {
-		close();
-		channelGroup = AsynchronousChannelGroup.withFixedThreadPool(16,
-				Executors.defaultThreadFactory());
-		server = AsynchronousServerSocketChannel.open(channelGroup).bind(
-				endPoint);
+        isOpen = true;
+        server.accept(server, new AcceptCompletionHandler());
+    }
 
-		isOpen = true;
-		server.accept(server, new AcceptCompletionHandler());
-	}
+    public void close() {
 
-	public void close() {
+        if (isOpen) {
+            isOpen = false;
+            if (server != null) {
+                if (server.isOpen())
+                    try {
+                        server.close();
+                    } catch (IOException e) {
+                    }
+                server = null;
+            }
 
-		if (isOpen) {
-			isOpen = false;
-			if (server != null) {
-				if (server.isOpen())
-					try {
-						server.close();
-					} catch (IOException e) {
-					}
-				server = null;
-			}
+            for (DataTransceivers dt : dataTransceiversQueue) {
+                dt.close();
+            }
 
-			for (DataTransceivers dt : dataTransceiversQueue) {
-				dt.close();
-			}
+            dataTransceiversQueue.clear();
 
-			dataTransceiversQueue.clear();
+            if (channelGroup != null) {
+                try {
+                    if (!channelGroup.isShutdown())
+                        channelGroup.shutdown();
 
-			if (channelGroup != null) {
-				try {
-					if (!channelGroup.isShutdown())
-						channelGroup.shutdown();
+                    if (!channelGroup.isTerminated())
+                        channelGroup.shutdownNow();
 
-					if (!channelGroup.isTerminated())
-						channelGroup.shutdownNow();
+                    channelGroup.awaitTermination(10, TimeUnit.SECONDS);
+                } catch (InterruptedException | IOException e) {
+                } finally {
+                    channelGroup = null;
+                }
+            }
+        }
+    }
 
-					channelGroup.awaitTermination(10, TimeUnit.SECONDS);
-				} catch (InterruptedException | IOException e) {
-				} finally {
-					channelGroup = null;
-				}
-			}
-		}
-	}
+    public String getServiceName() {
+        return serviceName;
+    }
 
-	public String getServiceName() {
-		return serviceName;
-	}
+    public InetSocketAddress getEndPointStr() throws IOException {
+        return (InetSocketAddress) server.getLocalAddress();
+    }
 
-	public InetSocketAddress getEndPointStr() throws IOException {
-		return (InetSocketAddress) server.getLocalAddress();
-	}
+    private class AcceptCompletionHandler
+            implements
+            CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel> {
 
-	private class AcceptCompletionHandler
-			implements
-			CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel> {
+        @Override
+        public void completed(AsynchronousSocketChannel clientChannel,
+                              AsynchronousServerSocketChannel serverChannel) {
 
-		@Override
-		public void completed(AsynchronousSocketChannel clientChannel,
-				AsynchronousServerSocketChannel serverChannel) {
+            final AsynchronousSocketChannel _clientChannel = clientChannel;
+            serverChannel.accept(serverChannel, this);
+            dataTransceiversQueue.add(new DataTransceivers(clientChannel,
+                    splitterFactory.create(), processorFactory.create(),
+                    new IChannelNeedToCloseHandler() {
 
-			final AsynchronousSocketChannel _clientChannel = clientChannel;
-			serverChannel.accept(serverChannel, this);
-			dataTransceiversQueue.add(new DataTransceivers(clientChannel,
-					splitterFactory.create(), processorFactory.create(),
-					new IChannelNeedToCloseHandler() {
+                        @Override
+                        public void needToCloseCallback(DataTransceivers sender) {
+                            if (_clientChannel.isOpen()) {
+                                try {
+                                    _clientChannel.shutdownInput();
+                                    _clientChannel.shutdownOutput();
+                                    _clientChannel.close();
+                                } catch (IOException e) {
+                                }
+                            }
+                            dataTransceiversQueue.remove(sender);
+                        }
+                    }, recvTimeout));
+        }
 
-						@Override
-						public void needToCloseCallback(DataTransceivers sender) {
-							if (_clientChannel.isOpen()) {
-								try {
-									_clientChannel.shutdownInput();
-									_clientChannel.shutdownOutput();
-									_clientChannel.close();
-								} catch (IOException e) {
-								}
-							}
-							dataTransceiversQueue.remove(sender);
-						}
-					}, recvTimeout));
-		}
-
-		@Override
-		public void failed(Throwable exc,
-				AsynchronousServerSocketChannel attachment) {
-			close();
-		}
-	}
+        @Override
+        public void failed(Throwable exc,
+                           AsynchronousServerSocketChannel attachment) {
+            logger.error("unknow exception...", exc);
+            close();
+        }
+    }
 }
