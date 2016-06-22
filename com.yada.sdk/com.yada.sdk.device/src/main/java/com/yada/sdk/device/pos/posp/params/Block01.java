d00ae7258626a8dc84762629e8c051b00161265c
liRenhao <renhao.li@bjyad.com>
2016-06-22 16:20:34 +0800

    Block01与测试的修改、Bolock02测试补全

--- com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/pos/posp/params/Block01.java
+++ com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/pos/posp/params/Block01.java
@@ -3,227 +3,133 @@ package com.yada.sdk.device.pos.posp.params;
 import java.nio.ByteBuffer;
 
 public class Block01 {
-  private byte[] flag;
-  private String additionInfo;
+    private static final int[] paramLengths = new int[]{1, 2, 1, 12, 4, 4, 3, 3, 3, 4, 16, 4, 16, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 2, 25, 20};
 
-  public Block01(String raw) {
-    flag = ByteBuffer.allocate(8).putLong(Long.parseLong(raw.substring(0, 8), 16) << 32).array();
-    additionInfo = raw.substring(8);
-  }
+    public final boolean useMAC;
+    public final boolean enable;
+    public final boolean autoCheck;
+    public final boolean allowBatchUpdate;
+    public final boolean allowReturn;
+    public final boolean allowManuallyEnterCardNo;
+    public final boolean allowMOTO;
+    public final boolean allOffline;
 
-  public boolean useMAC() {
-    return (flag[0] & 0X80) != 0;
-  }
+    public final boolean useTripleDES;
+    public final boolean allowBalanceQuery;
+    public final boolean allowPay;
+    public final boolean allowPreAuth;
+    public final boolean allowWithdrawals;
+    public final boolean allowAdjust;
+    public final boolean allowTip;
+    public final boolean allowInstallment;
 
-  public boolean enable() {
-    return (flag[0] & 0X40) != 0;
-  }
+    public final boolean allowIntegral;
+    public final boolean allowPaymentTransfer;
+    public final boolean allowCollections;
+    public final boolean useOper;
+    public final boolean allowCNChar;
+    public final boolean supportEMV;
+    public final boolean allowCardholderSelectApp;
+    public final boolean allowFallback;
 
-  public boolean autoCheck() {
-    return (flag[0] & 0X20) != 0;
-  }
+    public final boolean supportAppDownload;
+    public final boolean supportElectronicCash;
+    public final boolean allowAssignLoad;
+    public final boolean allowUnspecifiedLoad;
+    public final boolean allowFillBoardLoad;
 
-  public boolean allowBatchUpdate() {
-    return (flag[0] & 0X10) != 0;
-  }
+    public final int checkoutMethod;
+    public final int maxOfflineCount;
+    // 跳过一个备用标志
+    public final long maxOfflineAmt;
+    public final int maxAdjustAmtPercentage;
+    public final int maxTip;
+    public final String currency1;
+    public final String currency2;
+    public final String currency3;
+    public final String prefixOfDefaultPhone;
+    public final String defaultPhone;
+    public final String prefixOfSparePhone;
+    public final String sparePhone;
+    public final int billPrintCount;
+    public final String plant1;
+    public final String plant2;
+    public final String plant3;
+    public final String plant4;
+    public final String plant5;
+    public final String plant6;
+    public final String plant7;
+    public final String plant8;
+    public final String plant9;
+    public final int timeout;
+    public final String enMerName;
+    public final String cnMerName;
 
-  public boolean allowReturn() {
-    return (flag[0] & 0X08) != 0;
-  }
+    public Block01(String raw) {
+        byte[] flag = ByteBuffer.allocate(8).putLong(Long.parseLong(raw.substring(0, 8), 16) << 32).array();
+        String additionInfo = raw.substring(8);
 
-  public boolean allowManuallyEnterCardNo() {
-    return (flag[0] & 0X04) != 0;
-  }
+        useMAC = (flag[0] & 0X80) != 0;
+        enable = (flag[0] & 0X40) != 0;
+        autoCheck = (flag[0] & 0X20) != 0;
+        allowBatchUpdate = (flag[0] & 0X10) != 0;
+        allowReturn = (flag[0] & 0X08) != 0;
+        allowManuallyEnterCardNo = (flag[0] & 0X04) != 0;
+        allowMOTO = (flag[0] & 0X02) != 0;
+        allOffline = (flag[0] & 0X01) != 0;
 
-  public boolean allowMOTO() {
-    return (flag[0] & 0X02) != 0;
-  }
+        useTripleDES = (flag[1] & 0X80) != 0;
+        allowBalanceQuery = (flag[1] & 0X40) != 0;
+        allowPay = (flag[1] & 0X20) != 0;
+        allowPreAuth = (flag[1] & 0X10) != 0;
+        allowWithdrawals = (flag[0] & 0X08) != 0;
+        allowAdjust = (flag[1] & 0X04) != 0;
+        allowTip = (flag[1] & 0X02) != 0;
+        allowInstallment = (flag[1] & 0X01) != 0;
 
-  public boolean allOffline() {
-    return (flag[0] & 0X01) != 0;
-  }
+        allowIntegral = (flag[2] & 0X80) != 0;
+        allowPaymentTransfer = (flag[2] & 0X40) != 0;
+        allowCollections = (flag[2] & 0X20) != 0;
+        useOper = (flag[2] & 0X10) != 0;
+        allowCNChar = (flag[2] & 0X08) != 0;
+        supportEMV = (flag[2] & 0X04) != 0;
+        allowCardholderSelectApp = (flag[2] & 0X02) != 0;
+        allowFallback = (flag[2] & 0X01) != 0;
 
-  public boolean useTripleDES() {
-    return (flag[1] & 0X80) != 0;
-  }
+        supportAppDownload = (flag[3] & 0X80) != 0;
+        supportElectronicCash = (flag[3] & 0X20) != 0;
+        allowAssignLoad = (flag[3] & 0X10) != 0;
+        allowUnspecifiedLoad = (flag[3] & 0X08) != 0;
+        allowFillBoardLoad = (flag[3] & 0X04) != 0;
 
-  public boolean allowBalanceQuery() {
-    return (flag[1] & 0X40) != 0;
-  }
-
-  public boolean allowPay() {
-    return (flag[1] & 0X20) != 0;
-  }
-
-  public boolean allowPreAuth() {
-    return (flag[1] & 0X10) != 0;
-  }
-
-  public boolean allowWithdrawals() {
-    return (flag[1] & 0X08) != 0;
-  }
-
-  public boolean allowAdjust() {
-    return (flag[1] & 0X04) != 0;
-  }
-
-  public boolean allowTip() {
-    return (flag[1] & 0X02) != 0;
-  }
-
-  public boolean allowInstallment() {
-    return (flag[1] & 0X01) != 0;
-  }
-
-  public boolean allowIntegral() {
-    return (flag[2] & 0X80) != 0;
-  }
-
-  public boolean allowPaymentTransfer() {
-    return (flag[2] & 0X40) != 0;
-  }
-
-  public boolean allowCollections() {
-    return (flag[2] & 0X20) != 0;
-  }
-
-  public boolean useOper() {
-    return (flag[2] & 0X10) != 0;
-  }
-
-  public boolean allowCNChar() {
-    return (flag[2] & 0X08) != 0;
-  }
-
-  public boolean supportEMV() {
-    return (flag[2] & 0X04) != 0;
-  }
-
-  public boolean allowCardholderSelectApp() {
-    return (flag[2] & 0X02) != 0;
-  }
-
-  public boolean allowFallback() {
-    return (flag[2] & 0X01) != 0;
-  }
-
-  public boolean supportAppDownload() {
-    return (flag[3] & 0X80) != 0;
-  }
-
-  public boolean supportElectronicCash() {
-    return (flag[3] & 0X20) != 0;
-  }
-
-  public boolean allowAssignLoad() {
-    return (flag[3] & 0X10) != 0;
-  }
-
-  public boolean allowUnspecifiedLoad() {
-    return (flag[3] & 0X08) != 0;
-  }
-
-  public boolean allowFillBoardLoad() {
-    return (flag[3] & 0X04) != 0;
-  }
-
-  public int checkoutMethod() {
-    return additionInfo.charAt(0) - '0';
-  }
-
-  public int maxOfflineCount() {
-    return Integer.parseInt(additionInfo.substring(1, 3));
-  }
-
-  public long maxOfflineAmt() {
-    return Long.parseLong(additionInfo.substring(4, 16));
-  }
-
-  public int maxAdjustAmtPercentage() {
-    return Integer.parseInt(additionInfo.substring(16, 20));
-  }
-
-  public int maxTip() {
-    return Integer.parseInt(additionInfo.substring(20, 24));
-  }
-
-  public String currency1() {
-    return additionInfo.substring(24, 27);
-  }
-
-  public String currency2() {
-    return additionInfo.substring(27, 30);
-  }
-
-  public String currency3() {
-    return additionInfo.substring(30, 33);
-  }
-
-  public String prefixOfDefaultPhone() {
-    return additionInfo.substring(33, 37).trim();
-  }
-
-  public String defaultPhone() {
-    return additionInfo.substring(37, 53).trim();
-  }
-
-  public String prefixOfSparePhone() {
-    return additionInfo.substring(53, 57).trim();
-  }
-
-  public String sparePhone() {
-    return additionInfo.substring(57, 73).trim();
-  }
-
-  public int billPrintCount() {
-    return Integer.parseInt(additionInfo.substring(73, 75));
-  }
-
-  public String plant1() {
-    return additionInfo.substring(75, 79);
-  }
-
-  public String plant2() {
-    return additionInfo.substring(79, 83);
-  }
-
-  public String plant3() {
-    return additionInfo.substring(83, 87);
-  }
-
-  public String plant4() {
-    return additionInfo.substring(87, 91);
-  }
-
-  public String plant5() {
-    return additionInfo.substring(91, 95);
-  }
-
-  public String plant6() {
-    return additionInfo.substring(95, 99);
-  }
-
-  public String plant7() {
-    return additionInfo.substring(99, 103);
-  }
-
-  public String plant8() {
-    return additionInfo.substring(103, 107);
-  }
-
-  public String plant9() {
-    return additionInfo.substring(107, 111);
-  }
-
-  public int timeout() {
-    return Integer.parseInt(additionInfo.substring(111, 113));
-  }
-
-  public String enMerName() {
-    return additionInfo.substring(113, 138).trim();
-  }
-
-  public String cnMerName() {
-    return additionInfo.substring(138).trim();
-  }
+        int position = 0;
+        checkoutMethod = additionInfo.charAt(position) - '0';
+        position += paramLengths[0];
+        maxOfflineCount = Integer.parseInt(additionInfo.substring(position, position += paramLengths[1]));
+        // 跳过备用标志
+        position += paramLengths[2];
+        maxOfflineAmt = Long.parseLong(additionInfo.substring(position, position += paramLengths[3]));
+        maxAdjustAmtPercentage = Integer.parseInt(additionInfo.substring(position, position += paramLengths[4]));
+        maxTip = Integer.parseInt(additionInfo.substring(position, position += paramLengths[5]));
+        currency1 = additionInfo.substring(position, position += paramLengths[6]);
+        currency2 = additionInfo.substring(position, position += paramLengths[7]);
+        currency3 = additionInfo.substring(position, position += paramLengths[8]);
+        prefixOfDefaultPhone = additionInfo.substring(position, position += paramLengths[9]).trim();
+        defaultPhone = additionInfo.substring(position, position += paramLengths[10]).trim();
+        prefixOfSparePhone = additionInfo.substring(position, position += paramLengths[11]).trim();
+        sparePhone = additionInfo.substring(position, position += paramLengths[12]).trim();
+        billPrintCount = Integer.parseInt(additionInfo.substring(position, position += paramLengths[13]));
+        plant1 = additionInfo.substring(position, position += paramLengths[14]);
+        plant2 = additionInfo.substring(position, position += paramLengths[15]);
+        plant3 = additionInfo.substring(position, position += paramLengths[16]);
+        plant4 = additionInfo.substring(position, position += paramLengths[17]);
+        plant5 = additionInfo.substring(position, position += paramLengths[18]);
+        plant6 = additionInfo.substring(position, position += paramLengths[19]);
+        plant7 = additionInfo.substring(position, position += paramLengths[20]);
+        plant8 = additionInfo.substring(position, position += paramLengths[21]);
+        plant9 = additionInfo.substring(position, position += paramLengths[22]);
+        timeout = Integer.parseInt(additionInfo.substring(position, position += paramLengths[23]));
+        enMerName = additionInfo.substring(position, position += paramLengths[24]).trim();
+        cnMerName = additionInfo.substring(position, position + paramLengths[25]).trim();
+    }
 }
--- com.yada.sdk/com.yada.sdk.device/src/test/java/com/yada/sdk/device/pos/posp/params/Block02Test.java
+++ com.yada.sdk/com.yada.sdk.device/src/test/java/com/yada/sdk/device/pos/posp/params/Block02Test.java
@@ -1,24 +1,31 @@
 package com.yada.sdk.device.pos.posp.params;
 
 import com.payneteasy.tlv.HexUtil;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by cuitao-pc on 16/6/22.
- */
 public class Block02Test {
-  private Block02 block;
+    private Block02 block;
 
-  @Before
-  public void before() {
-    block = new Block02(HexUtil.parseHex("DF298184202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020080101000000200801010000003200100020202020202020202020202020202020202020202020202020202020202020202020202020202020DF25080220080101000000"));
-  }
+    @Before
+    public void before() {
+        block = new Block02(HexUtil.parseHex("DF298184202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020080101000000200801010000003200100020202020202020202020202020202020202020202020202020202020202020202020202020202020DF25080220080101000000"));
+    }
 
-  @Test
-  public void test() {
-    System.out.println(block.paramVersion);
-  }
+    @Test
+    public void test() {
+        Assert.assertEquals("0220080101000000", block.paramVersion);
+        Assert.assertEquals("", block.tmsfpuDefaultPhone);
+        Assert.assertEquals("", block.tmsfpuReservePhone);
+        Assert.assertEquals("", block.tmsfpuNetworkAddress);
+        Assert.assertEquals("", block.TmsfpuReserveNetworkAddress);
+        Assert.assertEquals("20080101000000", block.termDownloadDate);
+        Assert.assertEquals("20080101000000", block.termDownloadLimitDate);
+        Assert.assertEquals("2", block.communication);
+        Assert.assertEquals(10, block.downloadIdleTime);
+        Assert.assertEquals(0, block.tmsFlag);
+        Assert.assertEquals("", block.username);
+        Assert.assertEquals("", block.password);
+    }
 }
