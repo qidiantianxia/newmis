864aac78e1acb205178c3d0a01b0bb4f8ee87a03
cuitaocrazy <cuitaocrazy@163.com>
2016-06-22 11:29:16 +0800

    添加TLV库, 移除自己写的TLV

--- com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/pos/posp/params/Block02.java
+++ com.yada.sdk/com.yada.sdk.device/src/main/java/com/yada/sdk/device/pos/posp/params/Block02.java
@@ -1,23 +1,21 @@
 package com.yada.sdk.device.pos.posp.params;
 
-import com.yada.sdk.packages.comm.Tlv;
+import com.payneteasy.tlv.BerTag;
+import com.payneteasy.tlv.BerTlv;
+import com.payneteasy.tlv.BerTlvParser;
+import com.payneteasy.tlv.BerTlvs;
 
 /**
  * Created by cuitao-pc on 16/6/21.
  */
 public class Block02 {
-  private final Tlv[] params;
+  private final BerTlvs params;
 
   public Block02(byte[] raw) {
-    params = new Tlv(raw).getChildren();
+    params = new BerTlvParser().parse(raw);
   }
 
-  public Tlv get(byte[] tag) {
-    for(Tlv tlv : params) {
-      if(tlv.getTag()[0] == tag[0] && tlv.getTag()[1] == tag[1])
-        return tlv;
-    }
-
-    return null;
+  public BerTlv get(byte[] tag) {
+    return params.find(new BerTag(tag, 0, tag.length));
   }
 }
