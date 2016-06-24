228d43a6be494197d533fa33bcf7189f8c808fec
liRenhao <renhao.li@bjyad.com>
2016-06-24 15:44:50 +0800

    第四块参数国密公钥下载处理测试代码补全

--- com.yada.sdk/com.yada.sdk.device/src/test/java/com/yada/sdk/device/pos/posp/params/Block04_2Test.java
+++ com.yada.sdk/com.yada.sdk.device/src/test/java/com/yada/sdk/device/pos/posp/params/Block04_2Test.java
@@ -1,5 +1,7 @@
 package com.yada.sdk.device.pos.posp.params;
 
+import com.payneteasy.tlv.HexUtil;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,23 +11,22 @@ public class Block04_2Test {
 
     @Before
     public void before() {
-        String param04 = "";
-        // TODO 获得国密算法银联新公钥参数的报文后再补全测试
-//        block = new Block04_2(HexUtil.parseHex(param04));
+        String param04 = "DF350131DF3604010100009F0605A0000003339F220118DF37021230DF070104DF380111DF024037710FEB7CC3617767874E85509C268E8F931D68773E93A89F39A4247DFE2D280FC5BC838353885B6DAD447C8F90116BD9D314047591989F67F319544D42A48BDF3940DBDA8F27CF839323920DF931DC51943AE26A376F7EB2529F7233230B63CE15ABA76C1E508DA4A98675A2B522D2722BDF5CD93953973F241F568B282BB66CECDDDF280107DF25080420150428091009";
+        block = new Block04_2(HexUtil.parseHex(param04));
     }
 
     @Test
     public void test() {
-//        Assert.assertEquals("", HexUtil.toHexString(block.serviceId));
-//        Assert.assertEquals("", HexUtil.toHexString(block.rid));
-//        Assert.assertEquals((byte) 0x00, block.publicKeyStoreIndex);
-//        Assert.assertEquals((byte) 0x00, block.publicKeyIndex);
-//        Assert.assertEquals("", block.expiryDate);
-//        Assert.assertEquals((byte) 0x00, block.hashAlgorithmIndicator);
-//        Assert.assertEquals((byte) 0x00, block.publicKeyAlgorithmIndicator);
-//        Assert.assertEquals("", HexUtil.toHexString(block.publicKeyModulus));
-//        Assert.assertEquals("", HexUtil.toHexString(block.publicKeyHash));
-//        Assert.assertEquals("", block.paramVersion);
+        Assert.assertEquals("01010000", HexUtil.toHexString(block.serviceId));
+        Assert.assertEquals("A000000333", HexUtil.toHexString(block.rid));
+        Assert.assertEquals((byte) 0x07, block.publicKeyStoreIndex);
+        Assert.assertEquals((byte) 0x18, block.publicKeyIndex);
+        Assert.assertEquals("1230", block.expiryDate);
+        Assert.assertEquals((byte) 0x04, block.hashAlgorithmIndicator);
+        Assert.assertEquals((byte) 0x11, block.publicKeyAlgorithmIndicator);
+        Assert.assertEquals("37710FEB7CC3617767874E85509C268E8F931D68773E93A89F39A4247DFE2D280FC5BC838353885B6DAD447C8F90116BD9D314047591989F67F319544D42A48B", HexUtil.toHexString(block.publicKeyModulus));
+        Assert.assertEquals("DBDA8F27CF839323920DF931DC51943AE26A376F7EB2529F7233230B63CE15ABA76C1E508DA4A98675A2B522D2722BDF5CD93953973F241F568B282BB66CECDD", HexUtil.toHexString(block.publicKeyHash));
+        Assert.assertEquals("0420150428091009", block.paramVersion);
     }
 
 }
