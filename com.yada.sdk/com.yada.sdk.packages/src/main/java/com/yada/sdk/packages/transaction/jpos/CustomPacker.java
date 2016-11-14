bd7448e13865e30101bf3f7a2ada32d9a37e0611
fengming.jiang@bjyada.com <fengming.jiang@bjyada.com>
2016-11-14 10:01:11 +0800

    优化解包处理，并增加部分注释

--- com.yada.sdk/com.yada.sdk.packages/src/main/java/com/yada/sdk/packages/transaction/jpos/CustomPacker.java
+++ com.yada.sdk/com.yada.sdk.packages/src/main/java/com/yada/sdk/packages/transaction/jpos/CustomPacker.java
@@ -9,73 +9,67 @@ import com.yada.sdk.packages.transaction.IMessage;
 
 /**
  * 一个可定制的packer。
- * 
- * @author jiangfengming
- * 
+ *
  * @param <T>
+ * @author jiangfengming
  */
 public abstract class CustomPacker<T extends JposMessage> extends JposPacker {
-	CustomTranIdParser tranIdParser = new CustomTranIdParser();
+    CustomTranIdParser tranIdParser = new CustomTranIdParser();
 
-	/**
-	 * 
-	 * @param headLength
-	 *            头长度
-	 * @param path
-	 *            文件路径
-	 * @throws ISOException
-	 */
-	public CustomPacker(int headLength, String path) throws ISOException {
-		super(headLength, CustomPacker.class.getResourceAsStream(path), "");
-	}
+    /**
+     * @param headLength 头长度
+     * @param path       文件路径
+     * @throws ISOException JPOS异常
+     */
+    public CustomPacker(int headLength, String path) throws ISOException {
+        super(headLength, CustomPacker.class.getResourceAsStream(path), "");
+    }
 
-	/**
-	 * 
-	 * @param headLength
-	 *            头长度
-	 * @param path
-	 *            文件路径
-	 * @param realmName
-	 *            解析8583时日志中的realm名称
-	 * @throws ISOException
-	 */
-	public CustomPacker(int headLength, String path, String realmName) throws ISOException {
-		super(headLength, CustomPacker.class.getResourceAsStream(path), realmName);
-	}
+    /**
+     * @param headLength 头长度
+     * @param path       文件路径
+     * @param realmName  解析8583时日志中的realm名称
+     * @throws ISOException JPOS异常
+     */
+    public CustomPacker(int headLength, String path, String realmName) throws ISOException {
+        super(headLength, CustomPacker.class.getResourceAsStream(path), realmName);
+    }
 
-	@Override
-	public IMessage unpack(ByteBuffer byteBuffer) throws PackagingException {
-		IMessage message = super.unpack(byteBuffer);
-		((JposMessage) message).setTranIdParser(tranIdParser);
-		return message;
-	}
+    @Override
+    public IMessage unpack(ByteBuffer byteBuffer) throws PackagingException {
+        IMessage message = super.unpack(byteBuffer);
+        ((JposMessage) message).setTranIdParser(tranIdParser);
+        return message;
+    }
 
-	@Override
-	public JposMessage createEmpty() {
-		return generateJposMessage();
-	}
+    @Override
+    public JposMessage createEmpty() {
+        T msg = generateJposMessage();
+        msg.setPackager(super.packer);
+        return msg;
+    }
 
-	/**
-	 * 构建一个T类型的message。用于储存定制信息。
-	 * 
-	 * @return
-	 */
-	protected abstract T generateJposMessage();
+    /**
+     * 构建一个T类型的message。用于储存定制信息。
+     *
+     * @return JposMessage的实例
+     */
+    protected abstract T generateJposMessage();
 
-	/**
-	 * 生成一个交易唯一标志的方法
-	 * 
-	 * @param message
-	 * @return
-	 */
-	protected abstract String generateTranId(JposMessage message);
+    /**
+     * 生成一个交易唯一标志的方法
+     *
+     * @param message JposMessage的实例
+     * @return 交易唯一标识
+     */
+    protected abstract String generateTranId(JposMessage message);
 
-	private class CustomTranIdParser implements ITranIdParser {
+    private class CustomTranIdParser implements ITranIdParser {
 
-		@Override
-		public String getTranId(JposMessage message) {
-			return generateTranId(message);
-		}
-	}
+        @Override
+        public String getTranId(JposMessage message) {
+            return generateTranId(message);
+        }
+    }
 
 }
--- com.yada.sdk/com.yada.sdk.packages/src/main/java/com/yada/sdk/packages/transaction/jpos/JposPacker.java
+++ com.yada.sdk/com.yada.sdk.packages/src/main/java/com/yada/sdk/packages/transaction/jpos/JposPacker.java
@@ -1,5 +1,11 @@
 package com.yada.sdk.packages.transaction.jpos;
 
+import com.yada.sdk.packages.PackagingException;
+import com.yada.sdk.packages.transaction.IMessage;
+import com.yada.sdk.packages.transaction.IPacker;
+import org.jpos.iso.ISOException;
+import org.jpos.iso.packager.GenericPackager;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -7,90 +13,69 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-import org.jpos.iso.ISOException;
-import org.jpos.iso.ISOPackager;
-import org.jpos.iso.packager.GenericPackager;
-
-import com.yada.sdk.packages.PackagingException;
-import com.yada.sdk.packages.transaction.IMessage;
-import com.yada.sdk.packages.transaction.IPacker;
-
 /**
  * 通过JPOS实现的8583包的解析器
- * 
+ *
  * @author jiangfengming
- * 
  */
 public class JposPacker implements IPacker {
 
-	/**
-	 * JPOS提供的标准的8583解析器
-	 */
-	private GenericPackager packer = null;
+    /**
+     * JPOS提供的标准的8583解析器
+     */
+    protected GenericPackager packer = null;
 
-	/**
-	 * 头长度
-	 */
-	private int headLength;
-	/**
-	 * 
-	 * @param headLength
-	 *            头长度
-	 * @param filePath
-	 *            配置文件路径
-	 * @throws IOException
-	 * @throws ISOException
-	 */
-	public JposPacker(int headLength, String filePath) throws ISOException, IOException {
-		this(headLength, Files.newInputStream(Paths.get(filePath), StandardOpenOption.READ), "");
-	}
+    /**
+     * @param headLength 头长度
+     * @param filePath   配置文件路径
+     * @throws IOException 配置文件读取异常
+     * @throws ISOException JPOS异常
+     */
+    public JposPacker(int headLength, String filePath) throws ISOException, IOException {
+        this(headLength, Files.newInputStream(Paths.get(filePath), StandardOpenOption.READ), "");
+    }
 
-	/**
-	 * 
-	 * @param headLength
-	 *            头长度
-	 * @param inputStream
-	 *            配置文件输入流
-	 * @param realmName
-	 *            解析8583日志中的realm的名称
-	 * @throws ISOException
-	 */
-	public JposPacker(int headLength, InputStream inputStream, String realmName) throws ISOException {
-		this.headLength = headLength;
-		packer = new GenericPackager(inputStream);
-		packer.setHeaderLength(headLength);
-		org.jpos.util.Logger l = new org.jpos.util.Logger();
-		l.addListener(new JposLogListener());
-		packer.setLogger(l, realmName);
-	}
+    /**
+     * @param headLength  头长度
+     * @param inputStream 配置文件输入流
+     * @param realmName   解析8583日志中的realm的名称
+     * @throws ISOException JPOS异常
+     */
+    public JposPacker(int headLength, InputStream inputStream, String realmName) throws ISOException {
+        packer = new GenericPackager(inputStream);
+        packer.setHeaderLength(headLength);
+        org.jpos.util.Logger l = new org.jpos.util.Logger();
+        l.addListener(new JposLogListener());
+        packer.setLogger(l, realmName);
+    }
 
-	@Override
-	public ByteBuffer pack(IMessage message) throws PackagingException {
-		try {
-			JposMessage msg = (JposMessage) message;
-			return ByteBuffer.wrap(msg.pack());
-		} catch (Exception e) {
-			throw new PackagingException(e);
-		}
-	}
+    @Override
+    public ByteBuffer pack(IMessage message) throws PackagingException {
+        try {
+            JposMessage msg = (JposMessage) message;
+            return ByteBuffer.wrap(msg.pack());
+        } catch (Exception e) {
+            throw new PackagingException(e);
+        }
+    }
 
-	@Override
-	public IMessage unpack(ByteBuffer byteBuffer) throws PackagingException {
-		try {
-			byte[] bts = new byte[byteBuffer.remaining()];
-			byteBuffer.get(bts);
-			JposMessage message = createEmpty();
-			message.unpack(bts);
-			return message;
-		} catch (Exception e) {
-			throw new PackagingException(e);
-		}
-	}
+    @Override
+    public IMessage unpack(ByteBuffer byteBuffer) throws PackagingException {
+        try {
+            byte[] bts = new byte[byteBuffer.remaining()];
+            byteBuffer.get(bts);
+            JposMessage message = createEmpty();
+            message.unpack(bts);
+            return message;
+        } catch (Exception e) {
+            throw new PackagingException(e);
+        }
+    }
 
-	@Override
-	public JposMessage createEmpty() {
-		JposMessage message = new JposMessage();
-		message.setPackager(packer);
-		return message;
-	}
+    @Override
+    public JposMessage createEmpty() {
+        JposMessage message = new JposMessage();
+        message.setPackager(packer);
+        return message;
+    }
 }
